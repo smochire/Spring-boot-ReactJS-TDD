3afe423eb61dc754c52104299b95f4cfda2b4436
heriniaina <wiherakoto@gmail.com>
2023-08-26 10:07:44 +0300

    Initial commit

---
+++ src/main/java/com/heriniaina/data/LearnspringbootreactjsApplication.java
@@ -0,0 +1,13 @@
+package com.heriniaina.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringbootreactjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringbootreactjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heriniaina/data/LearnspringbootreactjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heriniaina.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringbootreactjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
